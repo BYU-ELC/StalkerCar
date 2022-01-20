e9d42d0a40e546fa04cacb00cb7c8aa0f4cb4927
Derekbenj <derekbenjward@gmail.com>
2022-01-20 14:48:15 -0700

    speed and direction succesfully transfered in both directions.

--- RaspiSerial/sketch_jan20a/sketch_jan20a.ino
+++ RaspiSerial/sketch_jan20a/sketch_jan20a.ino
@@ -1,10 +1,22 @@
+String data;
+int speed;
+int direction;
+
+
 void setup() {
   Serial.begin(9600);
 }
 void loop() {
   if (Serial.available() > 0) {
-    String data = Serial.readStringUntil('\n');
-    Serial.print("You sent me: ");
-    Serial.println(data);
+    
+    data = Serial.readStringUntil('\n');
+    speed     = data.substring(0,3).toInt();
+    direction = data.substring(3,6).toInt();
+    
+    Serial.print('(');
+    Serial.print(speed);
+    Serial.print(',');
+    Serial.print(direction);
+    Serial.println(')');
   }
 }
