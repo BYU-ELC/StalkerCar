16e32b579a19705853607b3019e87efec055e8b4
Derekbenj <derekbenjward@gmail.com>
2022-01-20 15:12:14 -0700

    created importable module for raspi-side writing speed and direction

---
+++ RaspiSerial/writeDataModule.py
@@ -0,0 +1,22 @@
+import serial
+import time
+
+def openSerialConnection():
+    ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
+    ser.reset_input_buffer()
+
+def writeData(data, serialPort):
+    speed, direction = data
+    serialPort.write(b"300100\n")
+        
+
+def readData(serialPort):
+    line = serialPort.readline().decode('utf-8').rstrip()
+    time.sleep(1)
+    return line
+
+if __name__ == '__main__':
+    ser = openSerialConnection()
+    while True:
+        writeData((100,300), ser)
+        print(readData(ser))
