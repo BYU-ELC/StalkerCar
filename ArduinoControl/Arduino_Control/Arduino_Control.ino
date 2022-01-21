c3ce0d0c8da1c498cb4d96bec9e9431921d185e5
matttrainor <matthewtrainor000@gmail.com>
2022-01-20 18:53:54 -0700

    updated control code

--- ArduinoControl/Arduino_Control/Arduino_Control.ino
+++ ArduinoControl/Arduino_Control/Arduino_Control.ino
@@ -1,24 +1,50 @@
-void setup() {
-  //set up steering servo pin
-  //set up motor esc control
-  
+//Control code for Arduino
 
+#include <servo.h>
+
+int degree;       
+int velocity;
+int oldDegree;
+int oldVelocity;
+
+int servoPin;
+int motorPin;
+int delayTime = 50;
+
+Servo Turner;
+Servo Motor;
+
+void setup() {
+  //set up steering servo and motor pins
+  Turner.attach(servoPin);
+  Motor.attach(motorPin);
 }
 
 void loop() {
 
+  //receive serial here and store in degree and velocity 
+  //degree between 0 and 180 ( > 90 right, < 90 left)
+  //go between 0 and 180 (> 90 forward, < backward)
 
-
-  //assume 3 digit for direction
-  //assum 3 digit for speed
-
-
-
-  
+  if (oldDegree == degree && oldVelocity == velocity) { }
+  else 
+  {
+    turn(degree);
+    go(velocity);
+    delay(delayTime);
+  }
 
 }
 
-//Function to turn right
-//Function to turn left
-//function to go forward
-//function to move backwards
+//Function to turn 
+void turn(int degree)
+{
+  Turner.write(degree);
+}
+
+
+//Function to control motor
+void go(int velocity)
+{
+  Motor.write(velocity);
+}
